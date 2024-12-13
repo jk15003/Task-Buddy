06b9036eb47f1a3783312a79912493ce80880261
jk15003 <josephkhoury321@gmail.com>
2024-12-13 11:15:16 -0500

    third commit

--- app/(platform)/(dashboard)/board/[boardId]/_components/list-container.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-container.tsx
@@ -9,82 +9,82 @@ import { useEffect, useState } from "react";
 import { ListItem } from "./list-item";
 import {
     DragDropContext,
-    Droppable
+    Droppable,
+    DropResult
 } from "@hello-pangea/dnd";
 import { toast } from "sonner";
 
-interface ListContainerProps{
-    data:ListWithCards[];
-    boardId:string;
+interface ListContainerProps {
+    data: ListWithCards[];
+    boardId: string;
 }
 
-function reorder<T>(list:T[],startIndex:number,endIndex:number){
+function reorder<T>(list: T[], startIndex: number, endIndex: number) {
     const result = Array.from(list);
-    const [removed]=result.splice(startIndex,1);
-    result.splice(endIndex,0,removed);
+    const [removed] = result.splice(startIndex, 1);
+    result.splice(endIndex, 0, removed);
 
     return result;
 };
 
-export const ListContainer=({
+export const ListContainer = ({
     data,
     boardId,
-}:ListContainerProps)=>{
-    const {execute:executeUpdateListOrder}=useAction(updateListOrder,{
-        onSuccess:()=>{
+}: ListContainerProps) => {
+    const { execute: executeUpdateListOrder } = useAction(updateListOrder, {
+        onSuccess: () => {
             toast.success("List reordered!");
         },
-        onError:(error)=>{
+        onError: (error) => {
             toast.error(error);
         }
     });
 
-    const {execute:executeUpdateCardOrder}=useAction(updateCardOrder,{
-        onSuccess:()=>{
+    const { execute: executeUpdateCardOrder } = useAction(updateCardOrder, {
+        onSuccess: () => {
             toast.success("Cards reordered!");
         },
-        onError:(error)=>{
+        onError: (error) => {
             toast.error(error);
         }
     });
 
-    const [orderedData,setOrderedData]=useState(data);
+    const [orderedData, setOrderedData] = useState(data);
 
-    useEffect(()=>{
+    useEffect(() => {
         setOrderedData(data);
-    },[data]);
+    }, [data]);
 
-    const onDragEnd=(result: any)=>{
-        const {destination,source,type}=result;
+    const onDragEnd = (result: DropResult) => {
+        const { destination, source, type } = result;
 
-        if(!destination){
+        if (!destination) {
             return;
         }
 
         //if dropped in the same position
-        if(
-            destination.droppableId === source.droppableId 
+        if (
+            destination.droppableId === source.droppableId
             && destination.index === source.index
-        ){
+        ) {
             return;
         }
 
         //user moving list
-        if(type==='list'){
-            const items=reorder(
+        if (type === 'list') {
+            const items = reorder(
                 orderedData,
                 source.index,
                 destination.index,
-            ).map((item,index)=>({...item,order:index}));
+            ).map((item, index) => ({ ...item, order: index }));
 
             setOrderedData(items);
-             //server action to save
-             executeUpdateListOrder({items,boardId});
-
+            //server action to save
+            executeUpdateListOrder({ items, boardId });
         }
 
-        if(type==="card"){
-            let newOrderedData=[...orderedData];
+        if (type === "card") {
+            const newOrderedData = [...orderedData];
 
             // Source and destination list
             const sourceList = newOrderedData.find(
@@ -94,86 +94,86 @@ export const ListContainer=({
                 (list) => list.id === destination.droppableId
             );
 
-            if(!sourceList || !destList){
+            if (!sourceList || !destList) {
                 return;
             }
             //check if cards exist on the source list
-            if(!sourceList.cards){
-                sourceList.cards=[];
+            if (!sourceList.cards) {
+                sourceList.cards = [];
             }
             //check if cards exist on the destList
-            if(!destList.cards){
-                destList.cards=[];
+            if (!destList.cards) {
+                destList.cards = [];
             }
             //moving the cards to te same list
-            if(source.droppableId===destination.droppableId){
-                const reorderedCards=reorder(
+            if (source.droppableId === destination.droppableId) {
+                const reorderedCards = reorder(
                     sourceList.cards,
                     source.index,
                     destination.index,
                 );
 
-                reorderedCards.forEach((card,idx)=>{
-                    card.order=idx;
+                reorderedCards.forEach((card, idx) => {
+                    card.order = idx;
                 });
 
-                sourceList.cards=reorderedCards;
+                sourceList.cards = reorderedCards;
 
                 setOrderedData(newOrderedData);
                 //server action trigger
                 executeUpdateCardOrder({
-                    boardId:boardId,
-                    items:reorderedCards,
+                    boardId: boardId,
+                    items: reorderedCards,
                 });
 
                 //user moves a card to another list
-            }else{
-                const[movedCard]=sourceList.cards.splice(source.index,1);
+            } else {
+                const [movedCard] = sourceList.cards.splice(source.index, 1);
 
                 //assign new list id to the moved card
-                movedCard.listId=destination.droppableId;
+                movedCard.listId = destination.droppableId;
                 //add card to the destination list
-                destList.cards.splice(destination.index,0,movedCard);
-                sourceList.cards.forEach((card,idx)=>{
-                    card.order=idx;
+                destList.cards.splice(destination.index, 0, movedCard);
+                sourceList.cards.forEach((card, idx) => {
+                    card.order = idx;
                 });
 
                 //update order for each card in the dest list
-                destList.cards.forEach((card,idx)=>{
-                    card.order=idx;
+                destList.cards.forEach((card, idx) => {
+                    card.order = idx;
                 });
 
                 setOrderedData(newOrderedData);
                 //server action
                 executeUpdateCardOrder({
-                    boardId:boardId,
-                    items:destList.cards,
+                    boardId: boardId,
+                    items: destList.cards,
                 })
             }
         }
     }
 
-    return(
+    return (
         <DragDropContext onDragEnd={onDragEnd}>
             <Droppable droppableId="lists" type="list" direction="horizontal">
-                {(provided)=>(
-                    <ol 
-                    {...provided.droppableProps}
-                    ref={provided.innerRef}
-                    className="flex gap-x-3 h-full"
+                {(provided) => (
+                    <ol
+                        {...provided.droppableProps}
+                        ref={provided.innerRef}
+                        className="flex gap-x-3 h-full"
                     >
-                        {orderedData.map((list, index)=>{
-                            return(
+                        {orderedData.map((list, index) => {
+                            return (
                                 <ListItem
-                                key={list.id}
-                                index={index}
-                                data={list}
+                                    key={list.id}
+                                    index={index}
+                                    data={list}
                                 />
                             );
                         })}
                         {provided.placeholder}
-                        <ListForm/>
-                        <div className="flex-shrink-0 w-1"/>
+                        <ListForm />
+                        <div className="flex-shrink-0 w-1" />
                     </ol>
                 )}
             </Droppable>
