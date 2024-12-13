e999566f552363659796ca591c6092bd04ee9870
jk15003 <josephkhoury321@gmail.com>
2024-12-13 11:33:52 -0500

    7th commit

--- app/(platform)/(dashboard)/board/[boardId]/layout.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/layout.tsx
@@ -3,67 +3,63 @@ import { auth } from "@clerk/nextjs/server";
 import { notFound, redirect } from "next/navigation";
 import { BoardNavBar } from "./_components/board-navbar";
 
-interface LayoutParams {
-    params: { boardId: string };
-}
-
 export async function generateMetadata({
-    params,
-}: LayoutParams): Promise<{ title: string }> {
-    const { orgId } = await auth();
-    const boardId = params.boardId;
-
-    if (!orgId) {
-        return {
-            title: "Board",
-        };
-    }
-
-    const board = await db.board.findUnique({
-        where: {
-            id: boardId,
-            orgId,
-        },
-    });
-
-    return {
-        title: board?.title || "Board",
-    };
-}
-
-export default async function BoardIdLayout({
-    children,
-    params,
+  params,
 }: {
-    children: React.ReactNode;
-    params: LayoutParams["params"];
+  params: { boardId: string };
 }) {
-    const { orgId } = await auth();
-    const boardId = params.boardId;
+  const { orgId } =await auth();
 
-    if (!orgId) {
-        redirect("/select-org");
-    }
+  if (!orgId) {
+    return {
+      title: "Board",
+    };
+  }
+  const board = await db.board.findUnique({
+    where: {
+      id: params.boardId,
+      orgId,
+    },
+  });
 
-    const board = await db.board.findUnique({
-        where: {
-            id: boardId,
-            orgId,
-        },
-    });
-
-    if (!board) {
-        notFound();
-    }
-
-    return (
-        <div
-            className="relative h-full bg-no-repeat bg-cover bg-center"
-            style={{ backgroundImage: `url(${board.imageFullUrl})` }}
-        >
-            <BoardNavBar data={board} />
-            <div className="absolute inset-0 bg-black/10" />
-            <main className="relative pt-28 h-full">{children}</main>
-        </div>
-    );
+  return {
+    title: board?.title || "Board",
+  };
 }
+
+const BoardIdLayout = async ({
+  children,
+  params,
+}: {
+  children: React.ReactNode;
+  params: { boardId: string };
+}) => {
+  const { orgId } =await auth();
+
+  if (!orgId) {
+    redirect("/select-org");
+  }
+
+  const board = await db.board.findFirst({
+    where: {
+      id: params.boardId,
+      orgId,
+    },
+  });
+
+  if (!board) {
+    notFound();
+  }
+
+  return (
+    <div
+      className="relative w-full h-full bg-no-repeat bg-center bg-cover"
+      style={{ backgroundImage: `url(${board.imageFullUrl})` }}>
+      <div className="absolute inset-0 bg-black/10" />
+      <BoardNavBar data={board} />
+      <main className="relative pt-28 h-full">{children}</main>
+    </div>
+  );
+};
+
+export default BoardIdLayout;
